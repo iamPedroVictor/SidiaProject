edb1763a909ae02d2afaf9442858a937c0fc2aef
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-21 22:59:34 -0400

    ScriptableObject de Vector3

---
+++ Assets/Scripts/Util/Variaveis/Reference/Vector3Reference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Vector3Reference
+{
+    public bool UsarConstante = true;
+    public Vector3 ConstanteValue;
+    public Vector3Variable Variable;
+
+    public Vector3 Value
+    {
+        get { return UsarConstante ? ConstanteValue : Variable.Value; }
+        set
+        {
+            if (UsarConstante)
+            {
+                this.ConstanteValue = value;
+            }
+            else
+            {
+                this.Variable.Value = value;
+            }
+        }
+    }
+}
