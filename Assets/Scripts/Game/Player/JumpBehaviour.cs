103310f2499371ffac572201df9ad10f25f3d49a
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-21 23:04:24 -0400

    Comportamento de pulo usa referencias dos dados

--- Assets/Scripts/Game/Player/JumpBehaviour.cs
+++ Assets/Scripts/Game/Player/JumpBehaviour.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class JumpBehaviour : MonoBehaviour
 {
     private Rigidbody rb;
+    private Collider collider;
     [SerializeField]
-    private Vector3 force = new Vector3(0, 1, 0);
+    private Vector3Reference force;
+    [SerializeField]
+    private FloatReference distanciaDoChao;
+    private float maxDistancia;
+    private bool isGround;
+
 
     private void Awake()
     {
         this.rb = GetComponent<Rigidbody>();
+        this.collider = GetComponent<Collider>();
+        maxDistancia = collider.bounds.extents.y + distanciaDoChao.Value;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        isGround = NoChao();
+        if (Input.GetMouseButtonDown(0) && isGround)
         {
-            rb.AddForce(this.force,ForceMode.Impulse);
+            rb.AddForce(this.force.Value,ForceMode.Impulse);
         }
     }
+
+    private bool NoChao()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, maxDistancia);
+    }
+
 }
