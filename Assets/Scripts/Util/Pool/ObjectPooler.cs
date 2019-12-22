378ec24552311924fba87ce997f090abcd9e72d8
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 03:09:44 -0400

    Pool Manager

---
+++ Assets/Scripts/Util/Pool/ObjectPooler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static ObjectPooler Instance;
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            GameObject parentPool = new GameObject(string.Format("Pool {0}", pool.tag));
+            parentPool.transform.SetParent(this.transform);
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for(int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.transform.SetParent(parentPool.transform);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Não existe uma pool com a tag: " + tag);
+            return null;
+        }
+        GameObject obj = poolDictionary[tag].Dequeue();
+        obj.SetActive(true);
+        obj.transform.position = position;
+        obj.transform.rotation = rotation;
+
+        IPoolObject poolObject = obj.GetComponent<IPoolObject>();
+
+        if (poolObject != null)
+        {
+            poolObject.OnSpawn();
+        }
+
+        poolDictionary[tag].Enqueue(obj);
+
+        return obj;
+    }
+
+    
+
+}
