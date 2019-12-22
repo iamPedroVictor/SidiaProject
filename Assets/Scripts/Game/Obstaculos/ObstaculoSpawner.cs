4c6818bc36c2409bb6fb07a3fa0042c4f69aba9c
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 03:10:17 -0400

    Spawner de Obstaculos.

---
+++ Assets/Scripts/Game/Obstaculos/ObstaculoSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaculoSpawner : MonoBehaviour
+{
+    
+    [SerializeField]
+    private FloatReference TempoMinimoParaSpawn;
+    [SerializeField]
+    private FloatReference TempoMaximoParaSpawn;
+    [SerializeField]
+    private Transform InicioTransform;
+
+    private float tempoSpawn = 2f;
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad > tempoSpawn)
+        {
+            float proximo = Random.RandomRange(TempoMinimoParaSpawn.Value, TempoMaximoParaSpawn.Value);
+            tempoSpawn += proximo;
+            Debug.Log(string.Format("Proximo: {0}", proximo));
+            ObjectPooler.Instance.SpawnFromPool("Box", InicioTransform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+
+}
