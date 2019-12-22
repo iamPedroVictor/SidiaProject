f9d88b4e40530af9fc716804013ff704e58dcd61
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-21 20:52:34 -0400

    Comportamento de pulo basico

---
+++ Assets/Scripts/Game/Player/JumpBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JumpBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private Vector3 force = new Vector3(0, 1, 0);
+
+    private void Awake()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(this.force,ForceMode.Impulse);
+        }
+    }
+}
