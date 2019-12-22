b1382aead5eb352767f496853b80695b0ebeeb47
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 00:08:03 -0400

    ObstaculosMovimento: Movimento simples pela horizontal para os obstaculos

---
+++ Assets/Scripts/Game/Obstaculos/ObstaculoMovimento.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class ObstaculoMovimento : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference velocidade;    
+
+
+    private void FixedUpdate()
+    {
+        transform.position = transform.position 
+            + new Vector3(velocidade.Value * Time.fixedDeltaTime, 0, 0);
+    }
+
+}
