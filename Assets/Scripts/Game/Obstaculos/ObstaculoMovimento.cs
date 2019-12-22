3d0ec80eb301eeb7f8fdac4fddcec6c01d50ae3b
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 03:09:02 -0400

    Movimento dos obstaculos é definido de forma randomica.

--- Assets/Scripts/Game/Obstaculos/ObstaculoMovimento.cs
+++ Assets/Scripts/Game/Obstaculos/ObstaculoMovimento.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Transform))]
-public class ObstaculoMovimento : MonoBehaviour
+public class ObstaculoMovimento : MonoBehaviour, IPoolObject
 {
     [SerializeField]
-    private FloatReference velocidade;    
+    private IntReference velocidadeMinima;
+    [SerializeField]
+    private IntReference velocidadeMaxima;
 
+    private int velocidade;
+
+    public void OnSpawn()
+    {
+        velocidade = getVelocidadeRandomica();
+    }
 
     private void FixedUpdate()
     {
         transform.position = transform.position 
-            + new Vector3(velocidade.Value * Time.fixedDeltaTime, 0, 0);
+            + new Vector3(velocidade * Time.fixedDeltaTime, 0, 0);
+    }
+
+    private int getVelocidadeRandomica() {
+        return Random.Range(velocidadeMinima.Value, velocidadeMaxima.Value);
     }
 
 }
