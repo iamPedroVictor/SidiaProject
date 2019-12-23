52d44e4550be810e1e5be45dab2e9e1567117f78
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 10:07:19 -0400

    IniciarParticulas: Componente para controlar ParticlesSytem

---
+++ Assets/Scripts/Game/VFX/IniciarParticulas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IniciarParticulas : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particle;
+
+    public void Iniciar()
+    {
+        particle.Play();
+    }
+
+}
