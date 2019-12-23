9e4d22acd14df9f3e875f7c455a94c3a83b3a777
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 00:38:24 -0400

    UIGameManager: Criado classe com o metodo para habilitar painel de game over

---
+++ Assets/Scripts/Util/UIGameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    private void Awake()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    public void HabilitarPainelGameOver() {
+        gameOverPanel.SetActive(true);
+    }
+}
