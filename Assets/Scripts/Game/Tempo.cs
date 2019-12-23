144dc702b3924168f9123d437cd7fdbcd612a259
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 01:18:48 -0400

    Tempo: Calcula o tempo da partida em segundos, servido como pontuação

---
+++ Assets/Scripts/Game/Tempo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tempo : MonoBehaviour
+{
+
+    [SerializeField]
+    private IntReference tempoDeJogo;
+    [SerializeField]
+    private BooleanReference isRunning;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning.Value)
+        {
+            tempoDeJogo.Value = (int)Time.timeSinceLevelLoad % 60;
+        }
+    }
+}
