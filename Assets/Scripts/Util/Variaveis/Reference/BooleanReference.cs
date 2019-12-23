fc9f708b809b90340896572205ba1a35e744a86f
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 23:41:08 -0400

    Variavel BooleanReference criada

---
+++ Assets/Scripts/Util/Variaveis/Reference/BooleanReference.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class BooleanReference
+{
+    public bool UsarConstante = true;
+    public bool ConstanteValue;
+    public BooleanVariable Variable;
+
+    public bool Value
+    {
+        get { return this.UsarConstante ? this.ConstanteValue : this.Variable.Value; }
+
+        set
+        {
+            if (this.UsarConstante)
+            {
+                this.ConstanteValue = value;
+            }
+            else
+            {
+                this.Variable.Value = value;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Util/Variaveis/ScriptableObjectBase/BooleanVariable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variaveis/Boolean")]
+public class BooleanVariable : ScriptableObject
+{
+    public bool Value;
+}
