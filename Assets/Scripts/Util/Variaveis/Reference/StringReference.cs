133863e4f40cba8f9f5a56d7289e08dd0c27c1d8
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 11:15:19 -0400

    Criado variavel String

---
+++ Assets/Scripts/Util/Variaveis/Reference/StringReference.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class StringReference
+{
+    public bool UsarConstante = true;
+    public string ConstanteValue;
+    public StringVariable Variable;
+
+    public string Value
+    {
+        get { return UsarConstante ? ConstanteValue : Variable.Value; }
+        set
+        {
+            if (UsarConstante)
+            {
+                this.ConstanteValue = value;
+            }
+            else
+            {
+                this.Variable.Value = value;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Util/Variaveis/ScriptableObjectBase/StringVariable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variaveis/String")]
+public class StringVariable : ScriptableObject
+{
+    public string Value;
+}
