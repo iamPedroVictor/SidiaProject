96acd06e5cc616f624bc30cec30153d308bcbd56
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 01:05:14 -0400

    Random

---
+++ Assets/Scripts/Game/UI/TextUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text textTempo;
+    [SerializeField]
+    private IntReference tempoDeJogo;
+
+    private void Update()
+    {
+        textTempo.text = tempoDeJogo.Value.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Game/UI/UIGameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameOverPanel;
+    [SerializeField]
+    private float segundosDeEspera;
+
+    private void Awake()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    public void HabilitarPainelGameOver() {
+        StartCoroutine("TempoDeEspera");
+        gameOverPanel.SetActive(true);
+    }
+
+    private IEnumerator TempoDeEspera()
+    {
+        yield return new WaitForSeconds(segundosDeEspera);
+    }
+    
+}
