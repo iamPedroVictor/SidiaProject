7ea74ec0c43e78f64ac98a1cf2b3d9b5de121e1a
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 00:31:15 -0400

    Player é arremessado para cima e é desabilitado

---
+++ Assets/Scripts/Game/Player/PlayerDie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDie : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3Reference force;
+    [SerializeField]
+    private string explosionTag;
+    private Rigidbody rb;
+    private float timeToDisable;
+    private bool die;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void DestroyPlayer()
+    {
+        ObjectPooler.Instance.SpawnFromPool(explosionTag, transform.position, transform.rotation);
+        die = true;
+        timeToDisable = Time.timeSinceLevelLoad + 2f;
+        rb.AddForce(force.Value, ForceMode.Impulse);
+        
+    }
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad > timeToDisable && die)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
