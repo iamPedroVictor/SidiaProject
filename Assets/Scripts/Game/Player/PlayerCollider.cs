2191ac27e4d9db9f319a26f185a63af614892f49
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 23:44:31 -0400

    PlayerCollider: Componente para cuidar de colisões

---
+++ Assets/Scripts/Game/Player/PlayerCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        IPlayerObstaculo obstaculo = collision.gameObject.GetComponent<IPlayerObstaculo>();
+        obstaculo?.TriggerPlayer();
+    }
+
+    public void Teste(string msg) {
+        Debug.Log(msg);
+        Time.timeScale = 0;
+    }
+}
+
