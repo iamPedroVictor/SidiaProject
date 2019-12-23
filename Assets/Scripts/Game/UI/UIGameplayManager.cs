277d7c5febbb48b61a4e1cdc2e666a104996b7a8
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 01:43:23 -0400

    Metodos para gerenciar a UI durante o gameplay

---
+++ Assets/Scripts/Game/UI/UIGameplayManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameplayManager : MonoBehaviour
+{
+    private GameObject gameplayPanel;
+    [SerializeField]
+    private IntReference partidasJogadas;
+    
+    void Awake()
+    {
+        if(partidasJogadas.Value < 3)
+        {
+            partidasJogadas.Value++;
+        }
+    }
+
+    public void DesabilitarGameplayUI()
+    {
+        gameplayPanel.SetActive(false);
+    }
+}
