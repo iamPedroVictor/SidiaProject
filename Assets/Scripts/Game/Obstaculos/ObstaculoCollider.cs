be73730b953d3b7ab14df670851aacb47faf3ca4
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-22 23:41:57 -0400

    Criado novas interfaces para os obstaculos e tratamento de colisões

---
+++ Assets/Scripts/Game/Obstaculos/ObstaculoCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaculoCollider : MonoBehaviour, IPlayerObstaculo, IDestroy
+{
+    [SerializeField]
+    private string poolTag;
+    [SerializeField]
+    private GameEvent gameOverEvent;
+
+    public void Destroy()
+    {
+        ObjectPooler.Instance.ReturnObjectToPool(poolTag, this.gameObject);
+    }
+
+    public void TriggerPlayer()
+    {
+        gameOverEvent.Raise();
+    }
+}
---
+++ Assets/Scripts/Game/Obstaculos/ObstaculoEnd.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ObstaculoEnd : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        IDestroy destroyGameObject = other.GetComponent<IDestroy>();
+        destroyGameObject?.Destroy();
+    }
+}
