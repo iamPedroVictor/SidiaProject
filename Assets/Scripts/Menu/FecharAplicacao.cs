73fd9bc67f6fb5a293b4d1fdca32bf2f214b6819
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-23 01:20:01 -0400

    Script para fechar a aplicação

---
+++ Assets/Scripts/Menu/FecharAplicacao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FecharAplicacao : MonoBehaviour
+{
+    public void Sair() {
+        Application.Quit();
+    }
+}
