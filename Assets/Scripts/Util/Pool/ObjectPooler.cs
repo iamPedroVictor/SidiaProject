53c4a6a4b6f8a3ad48085cf571b7f521d98ae1b2
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-25 17:20:50 -0400

    Metodo para criar novas pools de objetos

--- Assets/Scripts/Util/Pool/ObjectPooler.cs
+++ Assets/Scripts/Util/Pool/ObjectPooler.cs
@@ -9,35 +9,50 @@ public class ObjectPooler : MonoBehaviour
     {
         public StringReference tag;
         public GameObject prefab;
-        public int size;
+        public IntReference size;
     }
 
+    
     public static ObjectPooler Instance;
-    public List<Pool> pools;
-    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    [SerializeField]
+    private List<Pool> pools;
+    [SerializeField]
+    private Dictionary<string, Queue<GameObject>> poolDictionary;
 
     private void Awake()
     {
         Instance = this;
-    }
-    private void Start()
-    {
         poolDictionary = new Dictionary<string, Queue<GameObject>>();
 
         foreach (Pool pool in pools)
         {
-            GameObject parentPool = new GameObject(string.Format("Pool {0}", pool.tag.Value));
-            parentPool.transform.SetParent(this.transform);
-            Queue<GameObject> objectPool = new Queue<GameObject>();
-            for(int i = 0; i < pool.size; i++)
-            {
-                GameObject obj = Instantiate(pool.prefab);
-                obj.transform.SetParent(parentPool.transform);
-                obj.SetActive(false);
-                objectPool.Enqueue(obj);
-            }
-            poolDictionary.Add(pool.tag.Value, objectPool);
+            BuildPool(pool);
         }
+    }
+
+    public void NewPool(GameObject prefab, StringReference poolTag, IntReference size)
+    {
+        Pool newPool = new Pool();
+        newPool.prefab = prefab;
+        newPool.tag = poolTag;
+        newPool.size = size;
+        pools.Add(newPool);
+        BuildPool(newPool);
+    }
+
+    private void BuildPool(Pool pool)
+    {
+        GameObject parentPool = new GameObject(string.Format("Pool {0}", pool.tag.Value));
+        parentPool.transform.SetParent(this.transform);
+        Queue<GameObject> objectPool = new Queue<GameObject>();
+        for (int i = 0; i < pool.size.Value; i++)
+        {
+            GameObject obj = Instantiate(pool.prefab);
+            obj.transform.SetParent(parentPool.transform);
+            obj.SetActive(false);
+            objectPool.Enqueue(obj);
+        }
+        poolDictionary.Add(pool.tag.Value, objectPool);
     }
 
     public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
