c5128cab8b9d31fd92875c57d05d8124522481aa
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-24 21:53:02 -0400

    Interações no menu e Salvando o input desejado

---
+++ Assets/Scripts/Menu/LoadPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPanel : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+        
+    }
+
+    public void OpenPanel()
+    {
+        panel.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/Menu/SaveInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum INPUT_TYPE
+{
+    Human,
+    IA
+}
+
+public class SaveInput : MonoBehaviour
+{
+    public InputType humanInput;
+    public InputType iaInput;
+    public InputLoad inputLoad;
+    public UnityEvent loadSceneEvent;
+
+    public void SaveInputType(int inputType)
+    {
+        INPUT_TYPE input = (INPUT_TYPE)inputType;
+        if(input == INPUT_TYPE.Human)
+        {
+            inputLoad.inputChoise = humanInput;
+        }else{
+            inputLoad.inputChoise = iaInput;
+        }
+        loadSceneEvent?.Invoke();
+    }
+
+}
