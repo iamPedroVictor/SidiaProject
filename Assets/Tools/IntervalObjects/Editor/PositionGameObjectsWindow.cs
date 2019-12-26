5e120adb7b08f39431f17302b37bdf7eb91c09f7
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-26 00:43:18 -0400

    IntervalObjects Criado: Permite organizar a posição de gameobjects de uma cena atraves de um intervalo

---
+++ Assets/Tools/IntervalObjects/Editor/PositionGameObjectsWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace IntervalObjects.Editor
+{
+    public class PositionGameObjectsWindow : EditorWindow
+    {
+        private Transform[] transforms;
+        private Vector3 intervalValue;
+        private OrganizeGameObjects Organize = new OrganizeGameObjects();
+
+        [MenuItem("Sidia/Position GameObjects")]
+        static void Init()
+        {
+            PositionGameObjectsWindow window = (PositionGameObjectsWindow)GetWindow(typeof(PositionGameObjectsWindow));
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Set GameObjects position with interval", EditorStyles.boldLabel);
+            Rect position = EditorGUILayout.BeginVertical();
+            GUILayout.Label("Set Interval");
+            position.y += 30;
+            position.x += 30;
+            position.width = EditorGUIUtility.currentViewWidth - 50;
+            intervalValue = EditorGUI.Vector3Field(position, "", intervalValue);
+            position.y += 40;
+            GUILayout.Label("", GUILayout.Width(EditorGUIUtility.currentViewWidth), GUILayout.Height(50));
+            if (GUILayout.Button("Organize"))
+            {
+                Organize.SetInterval(transforms, intervalValue);
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+
+        private void OnSelectionChange()
+        {
+            transforms = Selection.transforms;
+        }
+    }
+}
---
+++ Assets/Tools/IntervalObjects/OrganizeGameObjects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace IntervalObjects
+{
+    public class OrganizeGameObjects
+    {
+        public void SetInterval(Transform[] transforms, Vector3 intervalValue)
+        {
+            for (int i = 1; i < transforms.Length; i++)
+            {
+                Vector3 before = transforms[i - 1].position;
+                Vector3 newPosition = before + intervalValue;
+                transforms[i].position = newPosition;
+            }
+        }
+    }
+}
+
