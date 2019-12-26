99d81e197df76146bd743d9f013446b80236a535
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-26 10:39:00 -0400

    Operações de leitura e salvamento de moedas

---
+++ Assets/Scripts/Game/Coin/CoinsLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsLoad : MonoBehaviour
+{
+    [SerializeField]
+    private IntReference moedasTotal;
+    [SerializeField]
+    private StringReference playerPrefsMoeda;
+
+    public void Awake()
+    {
+        moedasTotal.Value = PlayerPrefs.GetInt(playerPrefsMoeda.Value, 0);
+    }
+
+}
---
+++ Assets/Scripts/Game/Coin/CoinsSave.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsSave : MonoBehaviour
+{
+    [SerializeField]
+    private IntReference moedasTotal;
+    [SerializeField]
+    private StringReference playerPrefsMoeda;
+
+    public void SalvarMoedas(){
+        PlayerPrefs.SetInt(playerPrefsMoeda.Value, moedasTotal.Value);
+    }
+}
