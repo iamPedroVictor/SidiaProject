c27fa14ec9d2c1e889305a5b737d6818a20b7834
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-12-26 10:26:01 -0400

    Refatorando PositionGameObjects

--- Assets/Tools/IntervalObjects/Editor/PositionGameObjectsWindow.cs
+++ Assets/Tools/IntervalObjects/Editor/PositionGameObjectsWindow.cs
@@ -1,43 +1,64 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IntervalObjects.Editor
 {
     public class PositionGameObjectsWindow : EditorWindow
     {
+        private int indexOption = 0;
+        private string[] options = new[] { "Use fist GameObject position", "Define vector3 to position" };
+        private bool useVector3ToBasePosition = false;
+
+        private bool objectsSelected = false;
         private Transform[] transforms;
+
         private Vector3 intervalValue;
+        private Vector3 vector3BasePosition = Vector3.zero;
+
         private OrganizeGameObjects Organize = new OrganizeGameObjects();
 
         [MenuItem("Sidia/Position GameObjects")]
-        static void Init()
-        {
+        static void Init(){
             PositionGameObjectsWindow window = (PositionGameObjectsWindow)GetWindow(typeof(PositionGameObjectsWindow));
+            window.titleContent = new GUIContent("Reposition GameObjects");
             window.Show();
         }
 
-        private void OnGUI()
-        {
+        private void OnGUI(){
             GUILayout.Label("Set GameObjects position with interval", EditorStyles.boldLabel);
-            Rect position = EditorGUILayout.BeginVertical();
-            GUILayout.Label("Set Interval");
-            position.y += 30;
-            position.x += 30;
-            position.width = EditorGUIUtility.currentViewWidth - 50;
-            intervalValue = EditorGUI.Vector3Field(position, "", intervalValue);
-            position.y += 40;
-            GUILayout.Label("", GUILayout.Width(EditorGUIUtility.currentViewWidth), GUILayout.Height(50));
-            if (GUILayout.Button("Organize"))
-            {
-                Organize.SetInterval(transforms, intervalValue);
+            //position = OldDraw(position);
+            EditorGUILayout.LabelField("Set Interval");
+            intervalValue = EditorGUILayout.Vector3Field("",intervalValue);
+            EditorGUILayout.LabelField("Use Vector 3 to base position?");
+            indexOption = EditorGUILayout.Popup(indexOption, options);
+            useVector3ToBasePosition = (indexOption != 0);
+            if (useVector3ToBasePosition){
+                vector3BasePosition = EditorGUILayout.Vector3Field("", vector3BasePosition);
             }
-
-            EditorGUILayout.EndVertical();
+            if(GUILayout.Button("Reposition GameObjects")){
+                if (!objectsSelected){
+                    Debug.LogError("No GameObjects selected");
+                }else{
+                    if (useVector3ToBasePosition){
+                        Organize.SetInterval(transforms, intervalValue, vector3BasePosition);
+                    }else{
+                        Organize.SetInterval(transforms, intervalValue);
+                    }
+                }
+            }
         }
 
-        private void OnSelectionChange()
-        {
-            transforms = Selection.transforms;
+        private void OnSelectionChange(){
+            if(Selection.gameObjects.Length == 0){
+                objectsSelected = false;
+                Organize.Clean();
+            }else{
+                objectsSelected = true;
+                transforms = Organize.GetSelectedGameObjects(Selection.gameObjects);
+            }
         }
+
     }
 }
--- Assets/Tools/IntervalObjects/OrganizeGameObjects.cs
+++ Assets/Tools/IntervalObjects/OrganizeGameObjects.cs
@@ -1,16 +1,49 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace IntervalObjects
 {
     public class OrganizeGameObjects
     {
-        public void SetInterval(Transform[] transforms, Vector3 intervalValue)
+        private List<GameObject> gameObjectsList = new List<GameObject>();
+
+        public void SetInterval(Transform[] transforms, Vector3 intervalValue){
+            RepositionGameObjects(transforms, intervalValue);
+        }
+
+        public void SetInterval(Transform[] transforms, Vector3 intervalValue, Vector3 basePosition)
         {
-            for (int i = 1; i < transforms.Length; i++)
-            {
+            transforms[0].position = basePosition;
+            RepositionGameObjects(transforms, intervalValue);
+        }
+        public Transform[] GetSelectedGameObjects(GameObject[] gameObjectsArray)
+        {
+            PopulateList(gameObjectsArray, gameObjectsList);
+            Transform[] transforms = new Transform[gameObjectsList.Count];
+            for (int i = 0; i < gameObjectsList.Count; i++){
+                transforms[i] = gameObjectsList[i].transform;
+            }
+            return transforms;
+        }
+        
+        public void Clean(){
+            gameObjectsList.Clear();
+        }
+
+
+        private void RepositionGameObjects(Transform[] transforms, Vector3 intervalValue){
+            for (int i = 1; i < transforms.Length; i++){
                 Vector3 before = transforms[i - 1].position;
                 Vector3 newPosition = before + intervalValue;
                 transforms[i].position = newPosition;
+            }
+        }
+
+        private void PopulateList(GameObject[] gameObjectsArray, List<GameObject> list){
+            for (int i = 0; i < gameObjectsArray.Length; i++){
+                if (!list.Contains(gameObjectsArray[i])){
+                    list.Add(gameObjectsArray[i]);
+                }
             }
         }
     }
